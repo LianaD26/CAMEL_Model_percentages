42048f8c22ab45a6678c07d64c81b06965e5cc62
David <david.lopez.rivera.2003@gmail.com>
2025-09-07 20:20:30 -0500

    add frontend

--- API/app/main.py
+++ API/app/main.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI
-from app.routes.camel import router as camel_router
-from app.routes.cooperativa import router as cooperativa_router
-from app.routes.indicador import router as indicador_router
-from app.routes.registro import router as registro_router
+
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
     return{"message": "Bienvenido a mi API con FastAPI 🚀"}
-
-app.include_router(camel_router)
-app.include_router(cooperativa_router)
-app.include_router(indicador_router)
-app.include_router(registro_router)
-
 
 from app.routes.camel import router as camel_router
 from app.routes.cooperativa import router as cooperativa_router
