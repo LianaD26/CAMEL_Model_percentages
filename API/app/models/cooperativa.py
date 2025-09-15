6220b450e2eca3557fd0247da7f50ba4543507b8
LianaD26 <limadio.uni@gmail.com>
2025-09-15 18:26:06 -0500

    Fix errors

---
+++ API/app/models/cooperativa.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String, Float
+from app.database import Base
+
+class Cooperativa(Base):
+    __tablename__ = "cooperativa"
+
+    ID_cooperativa = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(250), index=True)
---
+++ API/app/models/registro.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float,ForeignKey
+from app.database import Base
+
+class Registro(Base):
+    __tablename__ = "registro"
+
+    ID_registro = Column(Integer, primary_key=True, index=True)
+    ID_indicador = Column(Integer, ForeignKey("indicador.ID_indicador"), index=True)
+    ID_cooperativa = Column(Integer, ForeignKey("cooperativa.ID_cooperativa"), index=True)
+    ano = Column(Integer, index=True)
+    mes = Column(Integer, index=True)
+    valor = Column(Float, index=True)
