b989529e7ef6473b5f0e640d4934897c446addf9
David <david.lopez.rivera.2003@gmail.com>
2025-10-05 02:08:14 -0500

    correction links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from "./pages/home";
 import Page2 from "./pages/page2";
 import './App.css';
@@ -10,6 +10,10 @@ function App() {
     return (
         <Router basename={basename}>
             <div className="App">
+                <nav style={{marginBottom: '20px'}}>
+                    <Link to="/" style={{marginRight: '10px'}}>Inicio</Link>
+                    <Link to="/page2">Page2</Link>
+                </nav>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/page2" element={<Page2 />} />
