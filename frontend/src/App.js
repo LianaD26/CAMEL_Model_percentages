752a31f6cc776cf3443495135f3dd85490156228
David <david.lopez.rivera.2003@gmail.com>
2025-10-05 02:13:40 -0500

    update ruoter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/home";
 import Page2 from "./pages/page2";
 import './App.css';
@@ -10,10 +10,7 @@ function App() {
     return (
         <Router basename={basename}>
             <div className="App">
-                <nav style={{marginBottom: '20px'}}>
-                    <Link to="/" style={{marginRight: '10px'}}>Inicio</Link>
-                    <Link to="/page2">Page2</Link>
-                </nav>
+                {/* ...aquí solo las rutas, el menú se gestiona en Menu.js */}
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/page2" element={<Page2 />} />
